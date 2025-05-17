03db0f8035399d2e15d2dad750615c07925666fb
Станислав Маяцкий <stasm226@gmail.com>
2025-05-17 21:39:57 +0500

    feat: add user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,138 @@
-# from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """
+    Определяет менеджера пользователей. Нужен для правильного создания пользователей и суперпользователей в кастомной
+    модели пользователя.
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Создаёт обычного пользователя.
+        :param email: Электронная почта пользователя
+        :param password: Пароль пользователя
+        :param extra_fields: Дополнительные поля пользователя
+        :return: Объект пользователя
+        """
+        if not email:
+            raise ValueError("Email обязателен")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Создаёт суперпользователя.
+        :param email: Электронная почта суперпользователя
+        :param password: Пароль суперпользователя
+        :param extra_fields: Дополнительные поля суперпользователя
+        :return: Объект суперпользователя
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        # Проверка, чтобы избежать ситуаций, когда суперпользователь создаётся без нужных прав
+        if not extra_fields["is_staff"]:
+            raise ValueError("У суперпользователя должно быть is_staff=True.")
+        if not extra_fields["is_superuser"]:
+            raise ValueError("У суперпользователя должно быть is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    """
+    Определяет модель пользователя.
+    Attributes:
+        first_name (str): Имя пользователя;
+         last_name (str): Фамилия пользователя;
+             phone (str): Телефон для связи;
+             email (str): Электронная почта пользователя;
+                    role: Роль пользователя, доступные значения: user, admin. Роли: user, admin;
+                   image: Аватарка пользователя;
+          password (str): Пароль пользователя;
+    """
+
+    # Комменты '# type: ignore[var-annotated]' для mypy - чтобы не требовал аннотаций типов
+    first_name = models.CharField(
+        max_length=30,
+        blank=True,
+        verbose_name="Имя",
+        help_text="Имя",
+    )  # type: ignore[var-annotated]
+
+    last_name = models.CharField(
+        max_length=30,
+        blank=True,
+        verbose_name="Фамилия",
+        help_text="Фамилия",
+    )  # type: ignore[var-annotated]
+
+    phone = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name="Номер телефона",
+        help_text="Укажите ваш номер телефона",
+    )  # type: ignore[var-annotated]
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email",
+        help_text="Электронная почта",
+        error_messages={"unique": "Пользователь с таким email уже существует."},
+    )  # type: ignore[var-annotated]
+
+    role = models.CharField(
+        max_length=20,
+        blank=True,
+        verbose_name="Роль",
+        help_text="Укажите вашу роль",
+    )  # type: ignore[var-annotated]
+
+    image = models.ImageField(
+        upload_to="avatars/",
+        verbose_name="Аватар",
+        help_text="Загрузите ваш аватар",
+        blank=True,
+        null=True,
+    )  # type: ignore[var-annotated]
+
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name="Активен",
+    )  # type: ignore[var-annotated]
+
+    is_staff = models.BooleanField(
+        default=False,
+        verbose_name="Сотрудник",
+    )  # type: ignore[var-annotated]
+
+    is_superuser = models.BooleanField(
+        default=False,
+        verbose_name="Суперпользователь",
+    )  # type: ignore[var-annotated]
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "first_name",
+        "last_name",
+        "phone",
+    ]  # поля, требуемые при создании пользователя (кроме email и пароля)
+
+    id: int  # Для mypy
+
+    def __str__(self):
+        """
+        Возвращает строковое представление пользователя.
+        :return: Строковое представление пользователя
+        """
+        return self.email
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
