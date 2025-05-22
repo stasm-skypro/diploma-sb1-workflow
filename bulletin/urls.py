d8ddbc2df96d933d4fa755961f4e2f0ad68727e9
Станислав Маяцкий <stasm226@gmail.com>
2025-05-22 23:17:50 +0500

    feat: add URL for review API endpoint

--- bulletin/urls.py
+++ bulletin/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 
 from .apps import BulletinConfig
-from .views import BulletinViewSet
+from .views import BulletinViewSet, ReviewViewSet
 
 app_name = BulletinConfig.name
 
 router = DefaultRouter()
 router.register(r"bulletins", BulletinViewSet, basename="bulletins")  # /api/bulletin/bulletins/ - CRUD
+router.register(r"reviews", ReviewViewSet, basename="reviews")  # /api/bulletin/reviews/ - CRUD
 
 urlpatterns = router.urls
