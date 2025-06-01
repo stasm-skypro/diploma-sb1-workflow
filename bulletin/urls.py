8d2e2b47fd075488edc400d6f9ad21e3528dbed4
Станислав Маяцкий <stasm226@gmail.com>
2025-06-01 21:09:15 +0500

    feat: add handle for /api/bulletin/bulletins/me/

--- bulletin/urls.py
+++ bulletin/urls.py
@@ -1,13 +1,21 @@
 # bulletin/urls.py
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
 
 from .apps import BulletinConfig
 from .views import BulletinViewSet, ReviewViewSet
 
 app_name = BulletinConfig.name
 
+# Основной роутер
 router = DefaultRouter()
 router.register(r"bulletins", BulletinViewSet, basename="bulletins")  # /api/bulletin/bulletins/ - CRUD
 router.register(r"reviews", ReviewViewSet, basename="reviews")  # /api/bulletin/reviews/ - CRUD
 
-urlpatterns = router.urls
+# Вложенный роутер для отзывов
+bulletins_router = NestedDefaultRouter(router, r"bulletins", lookup="bulletin")  # /api/bulletin/bulletins/<id>/
+bulletins_router.register(
+    r"reviews", ReviewViewSet, basename="bulletin-reviews"
+)  # /api/bulletin/bulletins/<id>/reviews/
+
+urlpatterns = router.urls + bulletins_router.urls
